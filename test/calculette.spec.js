4188f0fb2c877779a53ca30402e42b78f71d207e
WalyCISSE <74499751+WalyCISSE@users.noreply.github.com>
2022-02-02 13:42:41 +0100

    Add files via upload

--- test/calculette.spec.js
+++ test/calculette.spec.js
@@ -7,16 +7,25 @@ var Calculette = require('../calculette');
     // });
 
 describe('Calculette', function() {
-  describe('#constructor()', function() {
+  describe('#constructor()', function() {//{{{
 
     it('should return 0 by default', function() {
       let calc = new Calculette();
       assert.equal( calc.value(), 0 )
-    });
+    })
+  })//}}}
 
-  });
+  describe('#set()', function() {//{{{
+    it('it should work only with numbers', function() {
+      let calc = new Calculette();
+      const expectedError = new Error("Unable to handle non-numbers values")
 
-  describe('#set()', function() {
+      assert.throws(
+        () => { calc.set("toto") }, 
+        expectedError
+      )
+    })
+
     it('it should change the inner value of the calculette', function() {
       let calc = new Calculette();
       calc.set(100);
@@ -35,20 +44,40 @@ describe('Calculette', function() {
 
       assert.equal( final, 200 )
     });
-  });
+  })//}}}
 
-  describe('#reset()', function() {
-    // it should return 0 after reset
+  describe('#reset()', function() {//{{{
+    it('it should work only with numbers', function() {
+      let calc = new Calculette();
+      const expectedError = new Error("Unable to handle non-numbers values")
+
+      assert.throws(
+        () => { calc.set("toto") }, 
+        expectedError
+      )
+    })
+
     it('should return 0 after reset', function() {
-      calc = new Calculette();
+      let calc = new Calculette();
       assert.equal( calc.value(), 0  )
     });
-  });
 
+    // FIXME: test that reset is composable too
+  });//}}}
 
-  describe('#add()', function() {
+  describe('#add()', function() {//{{{
+    it('it should work only with numbers', function() {
+      let calc = new Calculette();
+      const expectedError = new Error("Unable to handle non-numbers values")
+
+      assert.throws(
+        () => { calc.set("toto") }, 
+        expectedError
+      )
+    })
+    //
     it('it should not change the value when we add 0', function() {
-      calc = new Calculette();
+      let calc = new Calculette();
       calc.set(100)
       calc.add(0) // added value
     
@@ -56,7 +85,7 @@ describe('Calculette', function() {
     });
  
     it('should changes the inner value by a difference of X', function() {
-      calc = new Calculette();
+      let calc = new Calculette();
       calc.set(100)
       let initial = calc.value();
       let added = 98;
@@ -65,82 +94,145 @@ describe('Calculette', function() {
 
       assert.equal( final - initial, added )
     })
-  })
 
-  describe('#multiply()', function() {
+    it('should allow composing functions', function() {
+      let calc = new Calculette()
+      calc.set(100).add(10)
+      assert.equal( calc.value(), 110)
+    })
+  })//}}}
+
+  describe('#multiply()', function() {//{{{
+    it('it should work only with numbers', function() {
+      let calc = new Calculette();
+      const expectedError = new Error("Unable to handle non-numbers values")
+
+      assert.throws(
+        () => { calc.set("toto") }, 
+        expectedError
+      )
+    })
+  
     it('should not change the value when we multiply with 1', function() {
-      calc = new Calculette();
+      let calc = new Calculette();
       calc.set(100)
       calc.multiply(1)
       assert.equal( calc.value(), 100 ) 
     })
    
     it('should change to 0 when we multiply with 0', function() {
-      calc = new Calculette();
+      let calc = new Calculette();
       calc.set(100)
       calc.multiply(0)
       assert.equal( calc.value(), 0 ) 
     })
 
     it('should work even with negative values', function() {
-      calc = new Calculette();
+      let calc = new Calculette();
       calc.set(100)
       calc.multiply(-1)
       assert.equal( calc.value(), -100 ) 
     })
    
     // it changes the inner value by +X 
-  });
+    //
+    it('should allow composing functions', function() {
+      let calc = new Calculette();
+      calc.reset().add(10).multiply(2)
+      assert.equal( calc.value(), 20)
 
-  it('should allow composing functions', function() {
-    calc = new Calculette()
-    calc.set(100).add(10)
-    assert.equal( calc.value(), 110)
+      calc.reset().multiply(2).add(10)
+      assert.equal( calc.value(), 10)
+    })
+  })//}}}
 
-    calc.reset().add(10).multiply(2)
-    assert.equal( calc.value(), 20)
+  describe('#substract()', function() {//{{{
+    it('it should work only with numbers', function() {
+      let calc = new Calculette();
+      const expectedError = new Error("Unable to handle non-numbers values")
 
-    calc.reset().multiply(2).add(10)
-    assert.equal( calc.value(), 10)
+      assert.throws(
+        () => { calc.set("toto") }, 
+        expectedError
+      )
+    })
+    //
+    it('should remove X from the internal value', function() { 
+      let calc = new Calculette()
+      calc.set(100)
+      calc.substract(10)
+      assert.equal( calc.value(), 90)
+    })
+
+    it('should be invariant when X is zero (0)', function() {
+      let calc = new Calculette()
+      calc.set(100)
+      calc.substract(0)
+      assert.equal( calc.value(), 100)
+    })
+
+    it('should be composable with other methods', function() { 
+      let calc = new Calculette()
+      calc.set(100).add(10).substract(10)
+      assert.equal( calc.value(), 100)
+
+      calc.set(100).substract(10).add(10)
+      assert.equal( calc.value(), 100)
+    })
+  })//}}}
+
+  describe('#divide()', function() {
+    it('it should work only with numbers', function() {
+      let calc = new Calculette();
+      const expectedError = new Error("Unable to handle non-numbers values")
+
+      assert.throws(
+        () => { calc.set("toto") }, 
+        expectedError
+      )
+    })
+
+    it('should divide the interval value by X', function() { 
+      let calc = new Calculette()
+      calc.set(100)
+      calc.divide(2)
+      assert.equal( calc.value(), 50)
+    })
+
+    it('should be invariant when X is one (1)', function() { 
+      let calc = new Calculette()
+      calc.set(100)
+      calc.divide(1)
+      assert.equal( calc.value(), 100)
+    })
+
+    it('should throw an exception when X is zero (0)', function() { 
+      let calc = new Calculette()
+      calc.set(100)
+
+      const expectedError = new Error("Unable to divide by zero")
+
+      assert.throws(
+        () => {
+          calc.divide(0)
+        },
+        expectedError
+      )
+    })
+
+    it('should be composable with other methods', function() { 
+      let calc = new Calculette()
+      calc.set(100).multiply(10).divide(10)
+      assert.equal( calc.value(), 100)
+
+      calc.set(100).divide(10).multiply(10)
+      assert.equal( calc.value(), 100)
+    })
   })
-
-
-/*test add*/
-
-/*describe('#add()', function() {
-  it('it should not change the value when we add 0', function() {
-    calc = new Calculette();
-    calc.set(100)
-    calc.add(0) // added value
-  
-    assert.equal( calc.value(), 100 )
-  });
-
-  it('should changes the inner value by a difference of X', function() {
-    calc = new Calculette();
-    calc.set(100)
-    let initial = calc.value();
-    let added = 98;
-    calc.add(added) // added value
-    let final = calc.value();
-
-    assert.equal( final - initial, added )
-  })
-})
-*/
-/*test add*/
-
-describe('#substract()', function() {
-  it('La méthode substract doit retirer x de la valeur interne de la calculette', function() {
-    calc = new Calculette();
-    calc.set(100)
-  })
-})
-
-
 
   describe('#value()', function() {
     // je vérifie que la méthode existe
-  });
+    // calc['value']
+  })
 
-});
+})
